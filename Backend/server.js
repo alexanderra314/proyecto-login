b82bec49c71b4d7c090666c8b209bee14aae8cbe
arodrigues <arodriguez@ipinnovatech.co>
2024-11-18 12:30:30 -0500

    bd update

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   ssl: true,
-  sslValidate: false,  // Desactiva la validación SSL
 });
 //mongoose.connect(process.env.MONGO_URI)
 
