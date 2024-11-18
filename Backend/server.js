c40f7178cfbc773aef3b7742a6dd87e0385eefa4
arodrigues <arodriguez@ipinnovatech.co>
2024-11-18 12:27:33 -0500

    conexion a la db

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 
 const app = express();
 
-//mongoose.connect(process.env.MONGO_URI, {
-//    useNewUrlParser: true,
-//    useUnifiedTopology: true,
-//})
-//.then(() => console.log('Conectado a MongoDB'))
-//.catch((error) => console.error('Error de conexión:', error));
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: false,  // Desactiva la validación SSL
+});
+//mongoose.connect(process.env.MONGO_URI)
 
 app.use(cors({
   origin: 'https://proyecto-login-alexanderra314.netlify.app', // URL del frontend
