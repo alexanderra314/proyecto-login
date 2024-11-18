36c1754950f4ab29fc6cbc36d7078174a01f8f5c
arodrigues <arodriguez@ipinnovatech.co>
2024-11-18 13:51:16 -0500

    qwertyu

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
+
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
